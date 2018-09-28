2e7d4475bd9415054f232e261a93d0ef2e2bf81d
Douglas Uretsky <douglasuretsky@gmail.com>
2018-09-28 19:29:02 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -14,11 +14,11 @@ class Person
   #   @name
   # end
   
-  def job=(new_job)
-    @job = new_job
-  end
+  # def job=(new_job)
+  #   @job = new_job
+  # end
   
-  def job
-    @job
-  end
+  # def job
+  #   @job
+  # end
 end
